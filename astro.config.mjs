7d785c1bf659da8cc1368357ceb822fb0c143a8b
tesao <shimeji9@gmail.com>
2023-02-25 00:11:29 +0900

    github pagesの対応

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://tessai9.github.io',
+  base: '/astro-hands-on',
 	integrations: [mdx(), sitemap()],
 });
